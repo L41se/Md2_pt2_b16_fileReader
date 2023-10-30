056521b131a1726dbe496532043ef845ee07829e
L41se <138740351+L41se@users.noreply.github.com>
2023-10-30 11:33:51 +0700

    Update ReadFileExample.java

--- Bai_1/ReadFileExample.java
+++ Bai_1/ReadFileExample.java
@@ -11,15 +11,9 @@ public class ReadFileExample {
 
     public void readFileText(String filePath) {
         try {
-            // Đọc file theo đường dẫn
             File file = new File(filePath);
-
-            // Kiểm tra nếu file không tồn tại thì ném ra ngoại lệ.
             if (!file.exists()) {
                 throw new FileNotFoundException();
-            }
-
-            // Đọc từng dòng của file và tiến hành cộng tổng lại
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line = "";
             int sum = 0;
@@ -28,15 +22,11 @@ public class ReadFileExample {
                 sum += Integer.parseInt(line);
             }
             br.close();
-
-            // Hiển thị ra màn hình tổng các số nguyên trong file
             System.out.println("Tổng = " + sum);
         } catch (Exception e) {
-            // TH file không tồn tại hoặc nội dung file có lỗi thì sẽ hiển thị thông báo lỗi.
             System.err.println("Fie không tồn tại or nội dung có lỗi!");
         }
     }
-
     public static void main(String[] args) {
         System.out.println("Nhập đường dẫn file: ");
         Scanner scanner = new Scanner(System.in);
